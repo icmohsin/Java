9780a6bec7a7c9036a7634be5d135fc4a0b02f79
icmohsin <35923135+icmohsin@users.noreply.github.com>
2019-01-15 13:29:35 +0100

    Add files via upload

---
+++ CharAndBoolean/src/com/monu/Main.java
@@ -0,0 +1,22 @@
+package com.monu;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //use unicode symbols...user unicode table weebsite
+        char mychar = '\u00A9';
+        System.out.println("Unicode outpout is  : " + mychar);
+
+        boolean myBollean = true;
+       // bollean isMale = true;
+
+        char myCharVar = '\u00Ae';
+        System.out.println(" the variable entered is : " + myCharVar);
+
+        String myString = "this is my string";
+        System.out.println("I entered this :" + myString );
+
+
+    }
+}
---
+++ DayofTheWeekChallange/src/DayofTheWeekChalange.java
@@ -0,0 +1,74 @@
+public class DayofTheWeekChalange {
+    public static void main(String[] args) {
+
+        printDayofTheWeek(-1);
+    }
+
+        private static void printDayofTheWeek (int day){
+            switch(day){
+                case 1:
+                    System.out.println("Sunday");
+                    break;
+                case 2:
+                    System.out.println("Monday");
+                    break;
+                case 3:
+                    System.out.println("Tuesday");
+                    break;
+                case 4:
+                    System.out.println("Wensday");
+                    break;
+                case 5:
+                    System.out.println("Thursay");
+                    break;
+                case 6:
+                    System.out.println("Friday");
+                    break;
+                case 7:
+                    System.out.println("Saturday");
+                    break;
+                    default:
+                        System.out.println("Invalid Day");
+                        break;
+            }
+        }
+
+        public static void printNumberInWord (int number) {
+        switch(number) {
+            case 0:
+                System.out.println("ZERO");
+                break;
+            case 1:
+                System.out.println("ONE");
+                break;
+            case 2:
+                System.out.println("TWO");
+                break;
+            case 3:
+                System.out.println("THREE");
+                break;
+            case 4:
+                System.out.println("FOUR");
+                break;
+            case 5:
+                System.out.println("FIVE");
+                break;
+            case 6:
+                System.out.println("SIX");
+                break;
+            case 7:
+                System.out.println("SEVEN");
+                break;
+            case 8:
+                System.out.println("EIGHT");
+                break;
+            case 9:
+                System.out.println("NINE");
+                break;
+            default:
+                System.out.println("OTHER");
+                break;
+
+        }
+        }
+}
