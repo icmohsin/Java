9b2e14cf2296792678a171370b8b003687c5fb11
icmohsin <icmohsin@gmail.com>
2019-01-17 21:12:04 +0100

    new

--- Passing_Value_from_String/src/com/monu/Main.java
+++ Passing_Value_from_String/src/com/monu/Main.java
@@ -4,7 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String numberAsString = "2018";
+        String numberAsString = "2018.125";
         System.out.println("number as string= " + numberAsString);
+
+        double number = Double.parseDouble(numberAsString);
+
+        System.out.println("intiger value is : "+ number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("numberAsString : " + numberAsString);
+        System.out.println("number : " + number);
     }
 }
